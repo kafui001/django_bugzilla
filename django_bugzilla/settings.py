2a7a879d1063fcf726fbc59d60ce76b94fd692cd
kafui001 <kafui01@yahoo.com>
2021-08-20 19:06:00 -0400

    Update settings.py

--- django_bugzilla/settings.py
+++ django_bugzilla/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
