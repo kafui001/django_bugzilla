48034818fb412746ac70965d3bed33a00b49e877
kafui001 <kafui01@yahoo.com>
2021-08-21 14:32:26 -0400

    new config file

--- django_bugzilla/settings.py
+++ django_bugzilla/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# config = os.path.join(BASE_DIR, 'config')
+
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
