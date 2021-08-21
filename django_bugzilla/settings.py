b3a2e0d33b690f111b41c9257e6c2442b91bc316
kafui001 <kafui01@yahoo.com>
2021-08-20 20:34:38 -0400

    add config at settings.py

--- django_bugzilla/settings.py
+++ django_bugzilla/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'django_bugzilla.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+config = os.path.join(BASE_DIR, 'config')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
